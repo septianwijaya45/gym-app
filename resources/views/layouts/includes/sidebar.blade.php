04bf585496dc9d3a48b1010c1962b0b502f91ba8
septianwijaya45 <septianwijaya170845@gmail.com>
2023-05-13 02:27:09 +0700

    change logo

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -2,9 +2,9 @@
  <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{route('dashboard')}}" class="brand-link">
-        <img src="" alt="Sanggar Senam Atheena" class="brand-image img-circle elevation-3"
+        <img src="{{ asset('img/icon/logo.png') }}" alt="Sanggar Senam Atheena" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">Sanggar Senam Atheena</span>
+        <span class="brand-text font-weight-light" style="font-size: 17px">Sanggar Senam Atheena</span>
     </a>
 
     <!-- Sidebar -->
